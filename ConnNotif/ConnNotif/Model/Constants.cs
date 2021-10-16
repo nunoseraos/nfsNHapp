70d0c791b90286f022538e9598ef8c9c43b70a83
nunoseraos <92627490+nunoseraos@users.noreply.github.com>
2021-10-16 10:53:09 +0100

    Create Constants.cs

---
+++ ConnNotif/ConnNotif/Model/Constants.cs
@@ -0,0 +1,16 @@
+namespace ConnNotif.Model
+{
+    public class Constants
+    {
+        public const string HubName = "notificationhub";
+        public const string HubConnectionString = "Endpoint=sb://......";
+        public const string HubTagName = "default";
+
+        //public string HubName { get; set; }
+        //public string HubConnectionString { get; set; }
+        //public string HubTagName { get; set; }
+
+
+
+    }
+}
