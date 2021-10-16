dc8f598f4db8c09ea199c8c7f617043b770053e9
nunoseraos <92627490+nunoseraos@users.noreply.github.com>
2021-10-16 10:53:34 +0100

    Add files via upload

---
+++ ConnNotif/ConnNotif/Model/INotificationsReceiver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConnNotif.Model
+{
+    public interface INotificationsReceiver
+    {
+        event EventHandler<string> ErrorReceived;
+
+        event EventHandler<string> NotificationReceived;
+
+        void RaiseErrorReceived(string message);
+
+        void RaiseNotificationReceived(string message);
+    }
+}
---
+++ ConnNotif/ConnNotif/Model/UserData.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConnNotif.Model
+{
+    public  class UserData
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string HubTag { get; set; }
+        [MaxLength(300)]
+        public string HubName { get; set; }
+
+        [MaxLength(300)] 
+        public string HubConnString { get; set; }
+    }
+}
